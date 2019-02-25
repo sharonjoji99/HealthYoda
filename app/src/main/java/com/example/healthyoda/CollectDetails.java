a74bf97db0ac85d1b4b86c4b2502397b7ec1f479
Rakshana J <47961988+rakshanaj@users.noreply.github.com>
2019-02-25 10:14:31 +0530

    Rakshana

---
+++ app/src/main/java/com/example/healthyoda/CollectDetails.java
@@ -0,0 +1,13 @@
+package com.example.healthyoda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CollectDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect_details);
+    }
+}
