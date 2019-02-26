42dc53160b7ed46b89dfc783187860313f3e6186
ShashankhS <shashankh.s22@gmail.com>
2019-02-26 08:56:01 +0530

    Added backend and ml

---
+++ html/login.php
@@ -0,0 +1,29 @@
+<?php
+	error_reporting(0);
+	$json = $_GET['json'];
+	$json = json_decode($json);
+
+	$phno = pg_escape_string($json->phno);
+	$password = hash("sha256", pg_escape_string($json->password));//pg_escape_string($json->password));
+	$con = pg_connect("host=localhost port=5432 dbname=healthyoda user=postgres password=xpert101");
+	$qry = "SELECT pass FROM tbl_user WHERE phno = '".$phno."'";
+	$select = pg_query($con, $qry);
+
+	if(pg_num_rows($select)){
+		$check = pg_fetch_row($select)[0];
+		if($password == $check){
+			$res->e_code = 0;
+			$res->e_msg = "User login successfull";
+		}
+		else{
+			$res->e_code = 1;
+			$res->e_msg = "Wrong user credentials";
+		}
+	}
+	else{
+		$res->e_code = 2;
+		$res->e_msg = "User not registered";
+	}
+
+	echo "[".json_encode($res)."]";
+?>
---
+++ html/python/check.py
@@ -0,0 +1,11 @@
+import sys
+import pickle
+knn_pkl = open('/var/www/html/python/knn.pkl','rb')
+knn = pickle.load(knn_pkl)
+a=int(sys.argv[1])
+b=int(sys.argv[2])
+c=int(sys.argv[3])
+d=int(sys.argv[4])
+e=int(sys.argv[5])
+f=int(sys.argv[6])
+print(knn.predict([[a,b,c,d,e,f]]))
